5c236c10cf212b34fc08ecd4863e2d1154b19a87
aditya26-09 <jatinmenghani300@gmail.com>
2021-04-02 16:45:30 +0530

    Folder re-structure

---
+++ NMService/Resources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  NMService
+//
+//  Created by Jatin Menghani on 02/04/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+     
+        return true
+    }
+}
+
