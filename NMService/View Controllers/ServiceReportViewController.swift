5020638f6e714fef2aa7edbe631a7d55e122de75
aditya26-09 <jatinmenghani300@gmail.com>
2021-04-06 21:48:13 +0530

    Added ServiceReportVC(DetailsVC) !

--- NMService/View Controllers/ServiceReportViewController.swift
+++ NMService/View Controllers/ServiceReportViewController.swift
@@ -1,24 +1,21 @@
 //
-//  ServiceListViewController.swift
+//  ServiceReportViewController.swift
 //  NMService
 //
-//  Created by Jatin Menghani on 05/04/21.
+//  Created by Jatin Menghani on 06/04/21.
 //
 
 import UIKit
 
-class ServiceListViewController: UIViewController {
+class ServiceReportViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var serviceList: [Service] = []
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupViews()
         registerCells()
-        callAPI()
     }
     
     func setupViews() {
@@ -34,56 +31,29 @@ class ServiceListViewController: UIViewController {
     func registerCells() {
         self.tableView.register(UINib(nibName: "HeaderCell", bundle: nil), forCellReuseIdentifier: "headerCell")
         self.tableView.register(UINib(nibName: "DateCell", bundle: nil), forCellReuseIdentifier: "dateCell")
-        self.tableView.register(UINib(nibName: "ServiceFilterCell", bundle: nil), forCellReuseIdentifier: "serviceFilterCell")
-        self.tableView.register(UINib(nibName: "ServiceDetailCell", bundle: nil), forCellReuseIdentifier: "serviceDetailCell")
     }
-    
-    func callAPI() {
-        NetworkController.shared.getServiceList { (result) in
-            switch result {
-            case .failure(let error):
-                print(error.localizedDescription)
-                return
-                
-            case .success(let serviceList):
-                DispatchQueue.main.async {
-                    self.serviceList = serviceList
-                    self.tableView.reloadData()
-                }
-            }
-        }
-    }
-    
+
     class func controller() -> UIViewController {
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "serviceListVC") as! ServiceListViewController
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "serviceReportVC") as! ServiceReportViewController
         return vc
     }
 }
 
-extension ServiceListViewController: UITableViewDelegate, UITableViewDataSource {
+extension ServiceReportViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 4
+        return 2
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0,1,2:
-            return 1
-            
-        case 3:
-            return self.serviceList.count
-            
-        default:
-            return 0
-        }
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.section {
         case 0:
             let headerCell = tableView.dequeueReusableCell(withIdentifier: "headerCell", for: indexPath) as! HeaderCell
-            headerCell.headerLabel.text = "Service List"
+            headerCell.headerLabel.text = "Service Details"
             return headerCell
         
         case 1:
@@ -92,18 +62,7 @@ extension ServiceListViewController: UITableViewDelegate, UITableViewDataSource 
             dateCell.dateButton.setTitle("Today, 03 April", for: .normal)
             dateCell.dateButton.isUserInteractionEnabled = false
             return dateCell
-            
-        case 2:
-            let serviceFilterCell = tableView.dequeueReusableCell(withIdentifier: "serviceFilterCell", for: indexPath) as! ServiceFilterCell
-            serviceFilterCell.totalItemsLabel.text = "\(self.serviceList.count) Total Items"
-            return serviceFilterCell
 
-        case 3:
-            let serviceDetailCell = tableView.dequeueReusableCell(withIdentifier: "serviceDetailCell", for: indexPath) as! ServiceDetailCell
-            serviceDetailCell.indexLabel.text = "\(indexPath.row + 1)"
-            serviceDetailCell.serviceItem = self.serviceList[indexPath.row]
-            return serviceDetailCell
-            
         default:
             return UITableViewCell()
         }
@@ -111,12 +70,10 @@ extension ServiceListViewController: UITableViewDelegate, UITableViewDataSource 
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         switch indexPath.section {
-        case 0,2:
+        case 0:
             return 50
         case 1:
             return 35
-        case 3:
-            return 100
         default:
             return 0
         }
@@ -124,7 +81,7 @@ extension ServiceListViewController: UITableViewDelegate, UITableViewDataSource 
     
     func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if indexPath.section == 0 {
-            navigationItem.title = "Service List"
+            navigationItem.title = "Service Details"
         }
     }
     
@@ -132,9 +89,5 @@ extension ServiceListViewController: UITableViewDelegate, UITableViewDataSource 
         if indexPath.section == 0 {
             navigationItem.title = ""
         }
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.navigationController?.pushViewController(ServiceReportViewController.controller(), animated: true)
     }
 }
